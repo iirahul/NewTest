79147fbdf5bc3c6d2149132a7ec6e8701e10dedb
Rahul K <irahul@live.com>
2021-04-22 14:56:13 +0530

    2nd commit

---
+++ Timethree/src/Substraction/Sub.java
@@ -0,0 +1,16 @@
+package Substraction;
+
+public class Sub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int veriable1 =10;
+		int veriable2 =4;
+		int result;
+		 
+		result=veriable1 -veriable2;
+		System.out.print(+result);
+
+	}
+
+}
