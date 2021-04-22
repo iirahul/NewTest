f368fb52ab4bfc28124cdc0d5945e9cbbdbffcb9
Rahul K <irahul@live.com>
2021-04-22 14:51:44 +0530

    First commit

---
+++ Timethree/src/Addition/Add.java
@@ -0,0 +1,18 @@
+package Addition;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+		int veriable1 =1;
+		int veriable2 =4;
+		int result;
+		 
+		result=veriable1 +veriable2;
+		System.out.print(+result);
+
+	}
+
+}
---
+++ Timethree/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author MSiLocal
+ *
+ */
+module Timethree {
+}
